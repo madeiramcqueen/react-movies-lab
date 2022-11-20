9304932e7d8c63cca629840bf6bc6af60b9dbeb8
madeiramcqueen <madeira@mcqueen.net>
2022-11-20 14:18:53 -0800

    Add NavBar to App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@ import './App.css';
 import { userState, useState } from 'react'
 import { Routes } from 'react-router-dom'
 import LoginPage from './pages/LoginPage/LoginPage';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   const [user, setUser] = useState(null)
 
   return (
-    <>
-      <div className="App">
-        <h1>React Movies Home Page</h1>
-        {user ?
-          <p>Welcome! You are signed in</p> :
-          <LoginPage />}
-      </div> </>
+    <div className="App">
+      <h1>React Movies Home Page</h1>
+      {user ?
+        <>
+          <NavBar />
+          <p>Welcome! You are signed in</p> </> :
+        <LoginPage />}
+    </div>
   );
 }
 
